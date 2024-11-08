3c74d50d7973b9201ba0498f203ed2ff446de71b
MarGoN <monondcosta@gmail.com>
2024-11-08 18:00:40 +0600

    problem 7 done

--- src/problem-7.ts
+++ src/problem-7.ts
@@ -11,4 +11,25 @@
     6 (assuming current year is 2024)
     
     */
+
+    class Car {
+
+        // constructor
+        constructor(public readonly name : string, public readonly model: string, public year: number){}
+
+        getCarAge(){
+            // get the current year
+            const date = new Date().getFullYear();
+
+            // difference between the current year and the car's year
+            let yearDiff = date - this.year;
+
+            console.log(yearDiff)
+
+            return yearDiff;
+        }
+    }
+
+    const car = new Car("Honda", "Civic", 2018);
+    car.getCarAge();
 }
